c106e7db1ca108f3cb2c6caf8c5c4ffd3b8e7704
ituitis-otal18 <hakan.otal@outlook.com>
2020-02-12 23:07:37 +0300

    Working app without login system

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,94 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="row">
+      <div class="mt-5 col-md-8 offset-md-2 text-center">
+        <h2>My List:</h2>
+        
+        <div id="text-container">
+          <hr>
+          <input type="text" v-model="Text">
+          <button @click="addTodo()" class="btn btn-primary ml-2">ADD</button>
+          <hr>
+        </div>
+        
+        <div id="list-container">
+          <ToDoList @deleteData="deleteData($event)" v-for="todo in List" :key="todo.id" :data="todo"/>
+        </div>
+
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import ToDoList from './components/Container.vue'
+  import axios from "axios"
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    name: 'app',
+    components : {
+      ToDoList
+    },
+    data(){
+      return {
+        Text : "",
+        List : []
+      }
+    },
+    methods : {
+      addTodo(){
+        axios.post("https://to-do-list-vue.firebaseio.com/List.json",{text : this.Text})
+        .then(response => {
+          console.log(response)
+          location.reload();
+        })
+        .catch(e => {
+          console.log(e)
+        })
+      },
+      deleteData(id){
+        console.log(id)
+        axios.delete("https://to-do-list-vue.firebaseio.com/List/"+id+".json")
+        .then(response => {
+          console.log(response)
+        })
+        .catch(e => {
+          console.log(e)
+        })
+        location.reload()
+      }
+    },
+    created(){
+      axios.get("https://to-do-list-vue.firebaseio.com/List.json")
+      .then(response => {  
+        for(let key in response.data){
+          let data = {
+            text: response.data[key].text,
+            id: key
+          }
+          this.List.push(data)
+          console.log(data)
+        }
+      })
+      .catch(e => {
+        console.log(e)
+      })
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  #app {
+    font-family: Arial, Helvetica, sans-serif;
+    color: white;
+  }
+  #text-container {
+    display: none
+  }
+  hr {
+    border: 1px dotted white;
+  }
+  .btn {
+  font-size:12px;
 }
 </style>
---
+++ src/components/Container.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <table class="mt-5 col-md-8 offset-md-2 text-center element">
+      <tr>
+        <th><a class="element pl-3 mt-2 md-2">{{ data.text }}</a></th>
+        <th class="element2"><button @click="$emit('deleteData', data.id)" class="btn btn-primary delete">DELETE</button></th>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ToDoList',
+  props: ["data"]
+}
+</script>
+
+<style>
+.element {
+  background-color: white;
+  color: black;
+  float: left;
+}
+.element2 {
+  display: block;
+  float: right;
+}
+.delete {
+  background-color: red;
+  display: none;
+  float: right;
+}
+.delete:hover {
+  background-color: darkred;
+}
+</style>
